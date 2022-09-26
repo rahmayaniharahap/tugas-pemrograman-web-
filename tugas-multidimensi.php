fb1a60bb2d6e2becb878e1473b7f25fefb77b4c2
rahmayaniharahap <114408114+rahmayaniharahap@users.noreply.github.com>
2022-09-26 19:48:35 +0700

    Add files via upload

---
+++ tugas-multidimensi.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+$country = array (
+  array("01101","01102","01103","01104"),
+);
+    
+for ($row = 0; $row < 1; $row++) {
+  echo "<p><b>NIM</b></p>";
+  for ($col = 0; $col <4; $col++) {
+    echo "<li>".$country[$row][$col]."</li>";
+  }
+  echo "</br>";
+}echo "</tr>";
+
+?>
+
+<?php
+$country = array (
+  array("81","72","61","93"),
+); 
+for ($row = 0; $row < 1; $row++) {
+  echo "<p><b>UTS</b></p>";
+  for ($col = 0; $col <4; $col++) {
+    echo "<li>".$country[$row][$col]."</li>";
+  }
+  echo "</br>";
+}echo "</tr>";
+
+?>
+
+<?php
+$country = array (
+  array("83","62","88","91"),
+); 
+for ($row = 0; $row < 1; $row++) {
+  echo "<p><b>UAS</b></p>";
+  for ($col = 0; $col <4; $col++) {
+    echo "<li>".$country[$row][$col]."</li>";
+  }
+  echo "</br>";
+}echo "</tr>";
+
+?>
+
+<?php
+$country = array (
+  array("77","63","89","90"),
+); 
+for ($row = 0; $row < 1; $row++) {
+  echo "<p><b>TUGAS</b></p>";
+  for ($col = 0; $col <4; $col++) {
+    echo "<li>".$country[$row][$col]."</li>";
+  }
+  echo "</br>";
+}echo "</tr>";
+
+?>
+
+<?php
+$country = array (
+  array("80,33","65,67","79,33","91,33"),
+); 
+for ($row = 0; $row < 1; $row++) {
+  echo "<p><b>Nilai Akhir</b></p>";
+  for ($col = 0; $col <4; $col++) {
+    echo "<li>".$country[$row][$col]."</li>";
+  }
+  echo "</br>";
+}echo "</tr>";
+
+?>
+
+</body>
+</html>
